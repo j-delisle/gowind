0f3c1bcb3ff7b87290031a8728c59414527962e2
Jay Delisle <j_delisle@my.uri.edu>
2020-09-25 23:53:31 -0400

    tailwind works and functinal

---
+++ assets/css/postcss.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    plugins: [
+        require('postcss-import')({
+            path: ["assets/css"]
+        }),
+        require('tailwindcss')('assets/css/tailwind.config.js'),
+        require('autoprefixer')
+    ]
+}
